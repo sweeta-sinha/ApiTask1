edeaac7dd56c7fa5ae380d263a93faf2a8920344
sweeta123 <63840245+sweeta123@users.noreply.github.com>
2020-04-28 13:38:45 +0530

    succesful form submit alert

--- WebApplication3/Models/User.cs
+++ WebApplication3/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,13 @@ namespace WebApplication3.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
